c46d8820cd36f99b2bf014cb560f1ff5acec58a3
ioseff-i <yusif.ibrahimov@york.ac.uk>
2025-02-13 08:25:23 +0000

    model_training

--- src/predict.py
+++ src/predict.py
@@ -77,3 +77,5 @@ if __name__=='__main__':
     
     decoded_text = tokenizer.decode(out.squeeze(0).tolist())
     print(decoded_text)
+    
+    
--- src/train.py
+++ src/train.py
@@ -11,6 +11,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join('..')))
 import argparse
 from model import GPTModel
+from utils import create_dataloader_v1
 
 
 GPT_CONFIG_124M = {
@@ -24,40 +25,95 @@ GPT_CONFIG_124M = {
 }
 
 torch.manual_seed(123)
-model = GPTModel(GPT_CONFIG_124M)
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch, target_batch = input_batch.to(device), target_batch.to(device)
+    logits = model(input_batch)
+    loss = F.cross_entropy(
+        logits.flatten(0,1),
+        target_batch.flatten()
+    )
+    return loss
+def calc_loss_loader(data_loader, model, device, num_batches = None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        num_batches = min(num_batches, len(data_loader))
+    
+    for i,(input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item()
+        else:
+            break
+    return total_loss / num_batches
+    
 
 
 
 
-from previous_chapters import create_dataloader_v1
-
-# Train/validation ratio
-train_ratio = 0.90
-split_idx = int(train_ratio * len(text_data))
-train_data = text_data[:split_idx]
-val_data = text_data[split_idx:]
 
 
-torch.manual_seed(123)
 
-train_loader = create_dataloader_v1(
-    train_data,
-    batch_size=2,
-    max_length=GPT_CONFIG_124M["context_length"],
-    stride=GPT_CONFIG_124M["context_length"],
-    drop_last=True,
-    shuffle=True,
-    num_workers=0
-)
+if __name__=='__main__':
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+        
+    model = GPTModel(GPT_CONFIG_124M).to(device)
 
-val_loader = create_dataloader_v1(
-    val_data,
-    batch_size=2,
-    max_length=GPT_CONFIG_124M["context_length"],
-    stride=GPT_CONFIG_124M["context_length"],
-    drop_last=False,
-    shuffle=False,
-    num_workers=0
-)
+
+    file_path = "../data/the-verdict.txt"
+    # file_path = "../data/fuzuli.txt"
+    with open(file_path, "r", encoding="utf-8") as file:
+        text_data = file.read()
+        
+        
+
+    # Train/validation ratio
+    train_ratio = 0.90
+    split_idx = int(train_ratio * len(text_data))
+    train_data = text_data[:split_idx]
+    val_data = text_data[split_idx:]
+
+
+    torch.manual_seed(123)
+
+    train_loader = create_dataloader_v1(
+        train_data,
+        batch_size=2,
+        max_length=GPT_CONFIG_124M["context_length"],
+        stride=GPT_CONFIG_124M["context_length"],
+        drop_last=True,
+        shuffle=True,
+        num_workers=0
+    )
+
+    val_loader = create_dataloader_v1(
+        val_data,
+        batch_size=2,
+        max_length=GPT_CONFIG_124M["context_length"],
+        stride=GPT_CONFIG_124M["context_length"],
+        drop_last=False,
+        shuffle=False,
+        num_workers=0
+    )
+    
+    with torch.no_grad():
+        train_loss = calc_loss_loader(
+            train_loader, model, device
+        )
+        val_loss = calc_loss_loader(
+            
+        )
+    print(f"Train loss: {train_loss:.4f}")
+    print(f"Validation loss: {val_loss:.4f}")
+    
+
 
 
